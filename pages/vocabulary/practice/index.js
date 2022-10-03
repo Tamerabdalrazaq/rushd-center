52beb12da04126d9e6258ccd2feaf0616cf3ab50
Tamer <tamerabdalrazaq@hotmail.com>
2022-10-04 01:01:58 +0300

    Client Side exception shot

--- pages/vocabulary/practice/index.js
+++ pages/vocabulary/practice/index.js
@@ -4,18 +4,30 @@ import { unstable_getServerSession } from 'next-auth'
 import { getUserData } from 'utils/api/client_api'
 import { authOptions } from 'pages/api/auth/[...nextauth]'
 
-function Practice({ session, userLists, categorizedWords, globalLists, categorizedLists }) {
+function Practice({
+   session,
+   userLists,
+   categorizedWords,
+   globalLists,
+   categorizedLists,
+}) {
    return (
       <div style={{ direction: 'ltr' }} className={`${styles.page} ccter`}>
-         <Quiz
-            data={{
-               session,
-               userLists,
-               categorizedWords,
-               categorizedLists,
-               globalLists,
-            }}
-         />
+         {session ? (
+            <Quiz
+               data={{
+                  session,
+                  userLists,
+                  categorizedWords,
+                  categorizedLists,
+                  globalLists,
+               }}
+            />
+         ) : (
+            <div style={{width: '100vw', height:'100vh'}} className='ccter'>
+               <h3 style={{fontSize: '8rem'}}>Please Sign In</h3>
+            </div>
+         )}
       </div>
    )
 }
@@ -34,7 +46,8 @@ export async function getServerSideProps(context) {
       }
    else {
       const data = await getUserData(session)
-      const { userLists, categorizedWords, globalLists, categorizedLists } = data
+      const { userLists, categorizedWords, globalLists, categorizedLists } =
+         data
       return {
          props: {
             session,
