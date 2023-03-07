fa10ca87f942b029f099f9c3679b4c34f47f4a48
Aver roes <tamerabdalrazaq@hotmail.com>
2023-03-07 03:28:03 +0200

    added quiz view definitions button

--- components/quiz/Question.js
+++ components/quiz/Question.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
-import styles from 'styles/practice.module.css'
-import WordDescribtion from './WordDescribtion'
-import { getDueTime, updateWordStatus } from 'utils/quiz_utils'
-import TextQuestion from './TextQuestion'
-import GridQuestion from './GridQuestion'
+import { useEffect, useState } from "react";
+import styles from "styles/practice.module.css";
+import WordDescribtion from "./WordDescribtion";
+import { getDueTime, updateWordStatus } from "utils/quiz_utils";
+import TextQuestion from "./TextQuestion";
+import Button from "../global/Button";
+import GridQuestion from "./GridQuestion";
 
 function Question({
    word,
@@ -14,38 +15,42 @@ function Question({
    wordsArray,
    gridQuiz,
 }) {
-   const [cheated, setCheated] = useState(false)
-   const [freeze, setFreeze] = useState(false)
-   const [answeredCorrectly, setAnsweredCorrectly] = useState(null)
+   const [cheated, setCheated] = useState(false);
+   const [freeze, setFreeze] = useState(false);
+   const [answeredCorrectly, setAnsweredCorrectly] = useState(null);
+   const [view_desc, setView_desc] = useState(false);
    useEffect(() => {
-      setAnsweredCorrectly(null)
-      setCheated(false)
-      setFreeze(false)
-   }, [questionNum])
+      setAnsweredCorrectly(null);
+      setCheated(false);
+      setFreeze(false);
+      setView_desc(false);
+   }, [questionNum]);
 
    function processSubmit(correctAnswer) {
-      if (freeze) return
+      if (freeze) return;
       if (correctAnswer) {
-         setAnsweredCorrectly(true)
-         setFreeze(true)
-         setTimeout(() => next(cheated), 1000)
+         setAnsweredCorrectly(true);
+         setFreeze(true);
+         setTimeout(() => next(cheated), 1000);
          if (!replicated) {
-            const nextPhase = cheated ? 0 : phase + 1
-            const dueTime = getDueTime(nextPhase)
-            updateWordStatus(parentId, word._id, dueTime, nextPhase)
+            const nextPhase = cheated ? 0 : phase + 1;
+            const dueTime = getDueTime(nextPhase);
+            updateWordStatus(parentId, word._id, dueTime, nextPhase);
          }
       } else {
-         const dueTime = getDueTime(0)
-         setAnsweredCorrectly(false)
-         setCheated(true)
-         if (!replicated) updateWordStatus(parentId, word._id, dueTime, 0)
+         const dueTime = getDueTime(0);
+         setAnsweredCorrectly(false);
+         setCheated(true);
+         if (!replicated) updateWordStatus(parentId, word._id, dueTime, 0);
       }
    }
 
    return (
       <div className={`${styles.questionBody} ccter`}>
          <div className={styles.word_wrapper}>
-            <h2 className={`${styles.questionHeading}`}>{word[gridQuiz?'word':'meaning']}</h2>
+            <h2 className={`${styles.questionHeading}`}>
+               {word[gridQuiz ? "word" : "meaning"]}
+            </h2>
          </div>
          {gridQuiz ? (
             <GridQuestion
@@ -65,10 +70,20 @@ function Question({
                questionNum={questionNum}
             />
          )}
-         <WordDescribtion word={word} visible={cheated} />
-         <div style={{ height: '2rem' }}></div>
+         {!cheated && !view_desc && (
+            <Button
+               text="View Definitions"
+               type="reddish"
+               onClick={() => {
+                  setView_desc(true);
+               }}
+            />
+         )}
+         <WordDescribtion word={word} visible={cheated || view_desc} />
+
+         <div style={{ height: "2rem" }}></div>
       </div>
-   )
+   );
 }
 
-export default Question
+export default Question;
